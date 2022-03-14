07142e674b67bba3e7e9c461792ec834ec764e90
Roylu7384 <roylu7384@gmail.com>
2022-03-14 14:01:56 -0400

    Add submit listener for forms to send ajax put or delete request

--- public/scripts/components/markerPopup.js
+++ public/scripts/components/markerPopup.js
@@ -5,38 +5,42 @@ $(() => {
     <div class='card'>
     <img class='card-img-top' src='${marker.img_url}'>
     <div class='card-body'>
-    <form>
+    <form class='infoDisplay'>
+    <input type="hidden" name="id" value="${marker.id}">
     <div class="form-group">
       <label for="title" class="col-form-label">Title</label>
       <div>
-        <input type="text" readonly class="form-control-plaintext" id="title" value='${marker.title}'>
+        <input type="text" readonly class="form-control-plaintext" id="title" name="titel" value='${marker.title}'>
       </div>
     </div>
     <div class="form-group">
       <label for="description" class="col-form-label">Description</label>
       <div>
-        <input type="text" readonly class="form-control-plaintext" id="description" value='${marker.description}'>
+        <input type="text" readonly class="form-control-plaintext" id="description" name="description" value='${marker.description}'>
       </div>
     </div>
     <div class="form-group hidden">
       <label for="img_url" class="col-form-label">Image url</label>
       <div>
-        <input type="text" readonly class="form-control-plaintext" id="img_url" value='${marker.img_url}'>
+        <input type="text" readonly class="form-control-plaintext" id="img_url" name="img_url" value='${marker.img_url}'>
       </div>
     </div>
     <div class="form-group hidden">
       <label for="icon_img_url" class="col-form-label">Icon Image url</label>
       <div>
-        <input type="text" readonly class="form-control-plaintext" id="icon_img_url" value='${marker.icon_img_url}'>
+        <input type="text" readonly class="form-control-plaintext" id="icon_img_url" name="icon_img_url" value='${marker.icon_img_url}'>
       </div>
     </div>
     ${isEdit ?
-    `<button type='submit'>Save changes</button>
-      <form>
-      <button type='submit'>Delete marker</button>
-      </form>`
-    : ``}
+        `<button type='submit'>Save changes</button>`
+        : ``}
     </form>
+    ${isEdit ?
+        `<form class='deleteMarker'>
+        <input type="hidden" name="id" value="${marker.id}">
+        <button type='submit'>Delete marker</button>
+        </form>`
+        : ``}
     </div>
     </div>
     `;
@@ -44,13 +48,44 @@ $(() => {
 
   window.createPopup = createPopup;
 
-  // test
+  // test marker
   const marker = {
+    id: 1,
     title: 'Pizza Nova',
     description: 'quick and easy',
     img_url: 'https://lh5.googleusercontent.com/p/AF1QipOqrdfgkc1dcj6XruqiT09zf1vD1Nd5MvxGfz3H=w408-h306-k-no',
     icon_img_url: 'https://icons.iconarchive.com/icons/sonya/swarm/64/Pizza-icon.png'
   };
 
-  window.$popUp = $(createPopup(marker, false));
+  window.$popUp = $(createPopup(marker, true));
+  // end of test
+
+  // Actions when submitting form elements
+  // submit changes
+  $('body').on('submit', '.infoDisplay', function(event) {
+    event.preventDefault();
+
+    const data = $(this).serialize();
+
+    $.ajax({
+      method: "PUT",
+      url: `/markers`, // id will be included inside data
+      data
+    });
+  });
+
+  // Delete marker
+  $('body').on('submit', '.deleteMarker', function(event) {
+    event.preventDefault();
+
+    const data = $(this).serialize();
+    console.log(data);
+    $.ajax({
+      method: "DELETE",
+      url: `/markers`, // id will be included inside data
+      data
+    });
+  });
+
+
 });
