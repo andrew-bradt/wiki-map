5fa39fcd1df34875ec5ddfe6db7ad831d80fbdf4
Roylu7384 <roylu7384@gmail.com>
2022-03-14 11:07:02 -0400

    Set up markers seeds with six sample markers

--- db/seeds/03_markers.sql
+++ db/seeds/03_markers.sql
@@ -1,4 +1,13 @@
 -- Markers table seeds here (Example)
--- INSERT INTO markers (owner_id, title, description)
--- VALUES
+INSERT INTO markers (map_id, title, description, img_url, icon_img_url, lat, lng)
+VALUES
+(1, 'Pizza Nova', 'quick and easy', 'https://lh5.googleusercontent.com/p/AF1QipOqrdfgkc1dcj6XruqiT09zf1vD1Nd5MvxGfz3H=w408-h306-k-no', 'https://icons.iconarchive.com/icons/sonya/swarm/64/Pizza-icon.png', 43.85995596252044, -79.48884687840145),
+(1, 'Antonino''s Pizzeria', 'Antonino Pizzeria is located off the main road. The staff are nice and friendly.  The food was great and would definitely recommend the place if you''re in the neighborhood.', 'https://lh5.googleusercontent.com/p/AF1QipO2aKdl-3vE57o6Y5H8QWLginX_BB9IhTw84WCj=w408-h306-k-no', 'https://icons.iconarchive.com/icons/sonya/swarm/64/Pizza-icon.png', 43.84111478497491, -79.49242457906516),
+(1, 'Vivo Pizza + Pasta', 'Cool casual atmosphere , good food, fair pricing and a good choice of wine. Pizza is amazing but also other dishes are not to be missed.', 'https://lh5.googleusercontent.com/p/AF1QipOc7TNBOJKVQytrVDf6CqA-vaxdvPAMxioFg94n=w408-h243-k-no', 'https://icons.iconarchive.com/icons/sonya/swarm/64/Pizza-icon.png', 43.84835461517154, -79.4577749510353),
+(2, 'Pronto Cafe', 'best espresso ice cream and food', 'https://lh5.googleusercontent.com/p/AF1QipMUEL7Y7YRkl1zMMNgk2SXmoq54RYx9fLWtSwYu=w408-h306-k-no', 'https://icons.iconarchive.com/icons/sonya/swarm/64/Ice-Cream-icon.png', 43.83576551082954, -79.50926864162265),
+(2, 'Chocolats Favoris', 'The place gets pretty busy, but the staff does an excellent job serving fast', 'https://lh5.googleusercontent.com/p/AF1QipOtH-CXs-Cjqj6i9Q3LLcA6-C7If0h8fw1ZSteG=w426-h240-k-no', 'https://icons.iconarchive.com/icons/sonya/swarm/64/Ice-Cream-icon.png', 43.86292436007062, -79.48542085881589),
+(2, 'Marble Slab', 'Small-batch ice cream is hand-blended with loads of mix-ins at this chain.', 'https://lh5.googleusercontent.com/p/AF1QipPedeLlwCspLCEj587drubUgvlr17Efy50FA4u2=w408-h306-k-no', 'https://icons.iconarchive.com/icons/sonya/swarm/64/Ice-Cream-icon.png', 43.81250193092914, -79.45334387918984);
+
+
+
 
