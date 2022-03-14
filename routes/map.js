309a6c5858ba6517ed5cf97f00239a93c73bdf9e
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-14 16:27:26 -0400

    return markers for GET /map/:id

--- routes/map.js
+++ routes/map.js
@@ -4,6 +4,16 @@ const router = express.Router();
 module.exports = (db) => {
   router.get('/:id', (req, res)=>{
     const {id} = req.params;
+    const queryParams = [id];
+    const queryString = `SELECT * FROM markers WHERE map_id = $1`;
+    return db.query(queryString, queryParams)
+      .then(data => {
+        res.json(data.rows);
+      })
+      .catch(err => {
+        res.status(500);
+        res.json({'msg': `${err.message}`});
+      });
   });
   return router;
 };
