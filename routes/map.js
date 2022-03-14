0805b6ac673d6ab7a7879c97e6da5618b3517d0d
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-14 16:06:02 -0400

    return router on map

--- routes/map.js
+++ routes/map.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-
+  router.get('/:id', (req, res)=>{
+    const {id} = req.params;
+  });
   return router;
 };
