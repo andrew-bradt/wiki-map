9627a39544c2a91a71c96f0c267de29b6690a25b
Roylu7384 <roylu7384@gmail.com>
2022-03-15 17:45:28 -0400

    Setup check out map button on map card to load corresponding map

--- public/scripts/components/mapCard.js
+++ public/scripts/components/mapCard.js
@@ -5,14 +5,22 @@ $(() => {
     <div class="card mapCard">
     <img class="card-img-top" src="${map.img_url}" alt="Card image cap">
     <div class="card-body">
+    <input type="hidden" value="${map.id}">
     <h5 class="card-title">${map.title}</h5>
     <p class="card-text">${map.description}</p>
-    <a href="#" class="btn btn-primary">Check out this map</a>
+    <button class="btn btn-primary checkOutMap">Check out this map</button>
     </div>
     </div>
     `;
   };
 
   window.mapsList.createMapCard = createMapCard;
+
+  $('main').on('click', '.checkOutMap', function() {
+
+    const mapId = $(this).siblings('input').val();
+    loadMap(mapId);
+
+  });
 
 });
