294819679ea9bba11ca3b7fe23e9137b7897b50a
Roylu7384 <roylu7384@gmail.com>
2022-03-15 13:04:23 -0400

    Fix bug that prevents maps list showing up on homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,6 @@ $(()=>{
   views_manager.load();
   getAllMaps().then(function( json ) {
     mapsList.addMapsList(json);
-    console.log(json);
-    views_manager.show('maps');
+    views_manager.show('$maps');
   });
 });
