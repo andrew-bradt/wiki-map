734328dcf47552d7a0da0c3da38c8bd5c51fae9f
Roylu7384 <roylu7384@gmail.com>
2022-03-16 10:36:59 -0400

    Refactor mapcard components to store map id to button directly

--- public/scripts/components/mapCard.js
+++ public/scripts/components/mapCard.js
@@ -5,10 +5,9 @@ $(() => {
     <div class="card mapCard">
     <img class="card-img-top" src="${map.img_url}" onerror="this.src='https://i.pinimg.com/originals/73/11/61/731161a7c8d5374b0fed6fcb43875c87.png'">
     <div class="card-body">
-    <input type="hidden" value="${map.id}">
     <h5 class="card-title">${map.title}</h5>
     <p class="card-text">${map.description}</p>
-    <button class="btn btn-primary checkOutMap">Check out this map</button>
+    <button class="btn btn-primary checkOutMap" data-id='${map.id}'>Check out this map</button>
     </div>
     </div>
     `;
@@ -18,7 +17,7 @@ $(() => {
 
   $('main').on('click', '.checkOutMap', function() {
 
-    const mapId = $(this).siblings('input').val();
+    const mapId = $(this).data('id');
     loadMap(mapId);
 
   });
