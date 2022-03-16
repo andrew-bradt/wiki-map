4a127d3bc10146f345b1b4a7f905f14b92569788
Roylu7384 <roylu7384@gmail.com>
2022-03-16 09:54:29 -0400

    Refactor map routes sequence

--- routes/map.js
+++ routes/map.js
@@ -2,6 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const queryParams = [id];
+    const queryString = `SELECT * FROM markers WHERE map_id = $1`;
+    return db.query(queryString, queryParams)
+      .then(data => {
+        res.json(data.rows);
+      })
+      .catch(err => {
+        res.status(500);
+        res.json({ 'msg': `${err.message}` });
+      });
+  });
+  router.post('/', (req, res) => {
+    const {title, description} = req.body;
+    const {user_id} = req.session;
+    const queryParams = [user_id, title, description];
+    const queryString = `
+      INSERT INTO maps (owner_id, title, description)
+      VALUES ($1, $2, $3)
+      RETURNING id;
+    `;
+    return db.query(queryString, queryParams)
+      .then(data => {
+        res.json(data.rows[0]);
+      })
+      .catch(err => {
+        res.status(500);
+        res.json({'msg': `${err.message}`});
+      });
+  });
+
   router.get('/', (req, res) => {
     const { user_id, seeFavorites, searchTitle } = req.query;
     const queryVal = [];
@@ -45,36 +78,5 @@ module.exports = (db) => {
   });
 
 
-  router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const queryParams = [id];
-    const queryString = `SELECT * FROM markers WHERE map_id = $1`;
-    return db.query(queryString, queryParams)
-      .then(data => {
-        res.json(data.rows);
-      })
-      .catch(err => {
-        res.status(500);
-        res.json({ 'msg': `${err.message}` });
-      });
-  });
-  router.post('/', (req, res) => {
-    const {title, description} = req.body;
-    const {user_id} = req.session;
-    const queryParams = [user_id, title, description];
-    const queryString = `
-      INSERT INTO maps (owner_id, title, description)
-      VALUES ($1, $2, $3)
-      RETURNING id;
-    `;
-    return db.query(queryString, queryParams)
-      .then(data => {
-        res.json(data.rows[0]);
-      })
-      .catch(err => {
-        res.status(500);
-        res.json({'msg': `${err.message}`});
-      });
-  });
   return router;
 };
