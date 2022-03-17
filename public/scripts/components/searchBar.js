22da8e86e7a635c5f7fd620fa8f8e08ee7c9a06c
Roylu7384 <roylu7384@gmail.com>
2022-03-17 11:26:40 -0400

    Implement searching by map title function

--- public/scripts/components/searchBar.js
+++ public/scripts/components/searchBar.js
@@ -1,6 +1,17 @@
 const $searchBar = $(`
-<form class="form-inline my-2 my-lg-0">
-  <input class="form-control mr-sm-2" type="search" placeholder="Search maps by title" aria-label="Search">
+<form class="searchBar form-inline my-2 my-lg-0">
+  <input class="form-control mr-sm-2" type="search" placeholder="Search maps by title" aria-label="Search" name="searchTitle">
 </form>
 `);
 
+$(() => {
+  $('header').on('submit', '.searchBar', function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+
+    getAllMaps(data).then(function( json ) {
+      mapsList.addMapsList(json);
+      views_manager.show('$maps');
+    });
+  });
+});
