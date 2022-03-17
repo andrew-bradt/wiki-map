5b46660f716df96b3e06b24c23d72da845b791ff
Roylu7384 <roylu7384@gmail.com>
2022-03-17 14:33:33 -0400

    Add more map and markers seeds

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,5 +1,6 @@
 -- Maps table seeds here (Example)
 INSERT INTO maps (owner_id, title, description)
 VALUES (1, 'Pizza', 'the only places i like to eat pizza at'),
-(2, 'Ice-cream', 'these icecream places are real good');
-
+(2, 'Ice-cream', 'these icecream places are real good'),
+(1, 'Chinatown eats', 'Try these when you are in downtown Toronto'),
+(2, 'Parks to hangout', 'popular parks that I know');
--- db/seeds/04_favorites.sql
+++ db/seeds/04_favorites.sql
@@ -2,5 +2,8 @@
 INSERT INTO favorites (user_id, map_id)
 VALUES
 (1, 1),
-(2, 2)
+(2, 2),
+(1, 3),
+(1, 4),
+(2, 1)
 ;
