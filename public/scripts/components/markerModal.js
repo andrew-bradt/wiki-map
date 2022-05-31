77806e41cf7e08b961da283fc383e4bf82326c26
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-31 11:41:06 -0400

    remove image from marker modal

--- public/scripts/components/markerModal.js
+++ public/scripts/components/markerModal.js
@@ -3,7 +3,6 @@ $(() => {
   const createModal = function (marker, isEdit) {
     return `
     <div class='card shadow p-3 mb-5 rounded' id='markerModal'>
-    <img class='card-img-top' src='${marker.img_url}' onerror="this.src='https://www.seekpng.com/png/detail/212-2120621_travel-places-emoticons-place-icon-png.png'">
     <div class='card-body'>
     <form class='infoDisplay'>
     <input type="hidden" name="id" value="${marker.id}">
